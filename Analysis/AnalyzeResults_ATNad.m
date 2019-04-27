25694074ef4637cfbe14c553725ff4891cd3e055
Laura Berkowitz <lberkowitz@unm.edu>
2019-04-27 10:40:39 -0600

    updated with new Data

--- Analysis/AnalyzeResults_ATNad.m
+++ Analysis/AnalyzeResults_ATNad.m
@@ -3,8 +3,8 @@
 addpath('D:\Users\BClarkLab\GoogleDrive\MatlabDir\BClarkToolbox\Analysis\Visualize')
 data=compileResults('F:\ClarkP30_Recordings\ProcessedData');
 
-control={'ATN04','ATN05','ATN09','ATN08','LB03'};%,'LB01','LB03','LB05'};
-transgenic={'ATN01','ATN03','ATN07','LB04'}; %,'LB02','LB04','LB06'};
+control={'LB03','LB05'};%,'LB01','LB03','LB05'};'ATN04','ATN05',,'LB05''LB03''ATN08'
+transgenic={'LB04','LB06'}; %,'LB02','LB04','LB06'};'ATN01','ATN03','LB04','LB06','ATN07','ATN09','ATN10','ATN14'
 
 %% COMPILE GROUPS
 data.control.measures=[];
@@ -21,7 +21,6 @@ for i=1:length(transgenic)
     data.transgenic.id=cat(1,data.transgenic.id,data.(transgenic{i}).id);
 end
 
-
 % COMPILE DATA
 group1=data.control.measures;
 group2=data.transgenic.measures;
@@ -34,9 +33,6 @@ varnames=data.varnames;
 group1(isinf(group1))=NaN;
 group2(isinf(group2))=NaN;
 
-% thetaFreq=CDFplots(group1(:,23,1),group2(:,23,1),{'Control','TgF344-AD'},varnames{23},1)
-% thetaFreq=CDFplots(group1(group1(:,24,1)<2500000,24,1),group2(:,24,1),{'Control','TgF344-AD'},varnames{24},1
-
 colstodelete=contains(varnames,["Displacement", "DisplacementCorr","DirectionalityIndex","nlaps","rateoverlap"...
     ,"fieldoverlap","lap_perm_stability","stabilityoverlaps","meanstability","spatialcorrelation"]);
 
@@ -45,147 +41,144 @@ group1(:,colstodelete,:)=[];
 group2(:,colstodelete,:)=[];
 
 
-%% PLACE CELL FILTER
-[group1place,group1placeid]=placefieldfilter(group1(:,:,:),group1id(:,:,1),varnames);
-[group2place,group2placeid]=placefieldfilter(group2(:,:,:),group2id(:,:,1),varnames);
-
-for i=1:length(varnames)
-    
-    all=CDFplots(group1place(:,i,1),group2place(:,i,1),{'Control','TgF344-AD'},varnames{i},2)
-    toPPT(gcf,'exportMode','matlab'); % default would be "exportFig" instead of "matlab"
-    toPPT('setTitle',all);
-    
-    close all
-    
-end
-
-% visualizecells(group1placeid,'WTplace')
-% visualizecells(group2placeid,'TGplace')
-
-% [h1,h2]=CoolHistogram(group1place(:,1),group2place(:,1),100,'InfoContent')
-% 
-group1place(:,24,3)=abs(wrapTo180(group1place(:,24,3)));
-group2place(:,24,3)=abs(wrapTo180(group2place(:,24,3)));
-
-AllStats=CDFplots(group1place(:,:,3),group2place(:,:,3),{'Control','TgF344-AD'},varnames,2)
-
-% done=PlotsStat_ATN(group1place(:,:,1:4),group2place(:,:,1:4),varnames)
-
-g1tuning=get_tuning(group1placeid);
-[g1tuning]=arrangenorm(g1tuning);
-fields=fieldnames(g1tuning);
-h = fspecial('gaussian');
-fig=figure;
- fig.Color=[1 1 1];
-    p = panel(fig); 
-    p.pack(2, 4); 
-    p.de.margin = 4;
-   
-for i=1:length(fields)
-p(1, i).select(); 
-ax1=gca; 
-imagesc(filter2(h, g1tuning.(fields{i}))); colormap(ax1,parula);
-axis off;axis tight
-end
-for i=1:4
-p(2, i).select(); 
-ax2=gca; 
-imagesc(g1tuning.(fields{i})==1); colormap(ax2,flipud(gray));
-axis off;axis tight
-end
-
-
-g2tuning=get_tuning(group2placeid);
-[g2tuning]=arrangenorm(g2tuning);
-fields=fieldnames(g2tuning);
-h = fspecial('gaussian');
-fig=figure;
- fig.Color=[1 1 1];
-    p = panel(fig); 
-    p.pack(2, 4); 
-    p.de.margin = 4;
-for i=1:length(fields)
-p(1, i).select(); 
-ax1=gca; 
-imagesc(filter2(h, g2tuning.(fields{i}))); colormap(ax1,parula);
-axis off;axis tight
-end
-for i=1:4
-p(2, i).select(); 
-ax2=gca; 
-imagesc(g2tuning.(fields{i})==1); colormap(ax2,flipud(gray));
-axis off;axis tight
-end
-
-
 %% HD Filter 
 [group1HD,group1HDid]=headdirectionfilter(group1(:,:,:),group1id(:,:,1),varnames);
 [group2HD,group2HDid]=headdirectionfilter(group2(:,:,:),group2id(:,:,1),varnames);
-% 
-% visualizecells(group1HDid,'WTHD')
-% visualizecells(group2HDid,'TGHD')
 
-% [h1,h2]=CoolHistogram(group1HD(:,9),group2HD(:,9),100,'Mean Vector Length')
-% 
-% AllStats=CDFplots(group1HD(:,:,1),group2HD(:,:,1),{'Control','TgF344-AD'},varnames,1)
+AllStats=CDFplots(group1HD(:,:,1),group2HD(:,:,1),{'Control','TgF344-AD'},varnames,1);
 
-
-
-%% INTERNEURON FILTER
-[group1inter,group1interid]=interneuronfilter(group1,group1id,varnames)
-[group2inter,group2interid]=interneuronfilter(group2,group2id,varnames)
-
-[h1,h2]=CoolHistogram(group1inter(:,34),group2inter(:,34),100,'thetaindex')
-
-% AllStats=CDFplots(group1inter(:,:,1),group2inter(:,:,1),{'Control','TgF344-AD'},varnames,1)
-
-group1autocorr=popVecAutocorr(group1interid);
-[g1thetaindex_sorted,I]=sort(group1inter(:,34,1));
-g1autocorrPopvec=group1autocorr(I,:);
-g1autocorrPopvec(sum(isnan(g1autocorrPopvec),2)>0,:)=[];
-
-group2autocorr=popVecAutocorr(group2interid);
-[g2thetaindex_sorted,I]=sort(group2inter(:,34,1));
-g2autocorrPopvec=group2autocorr(I,:);
-g2autocorrPopvec(sum(isnan(g2autocorrPopvec),2)>0,:)=[];
+tuning1=get_tuning(group1HDid);
+tuning2=get_tuning(group2HDid);
 
 
 
 
-% figure; subplot(1,2,1); imagesc(g1autocorrPopvec); 
-% 
-% subplot(1,2,2);   imagesc(g2autocorrPopvec); 
 
-fig=figure;fig.Color=[1 1 1];
-fig.OuterPosition=[1 6 960 1052];
-set(fig,'defaultAxesColorOrder',[0 0 0]);
-subplot(1,2,1);
-imagesc(g1autocorrPopvec);
-box off;axis xy
-ylabel('Cells')
-xlabel('Lag (ms)')
-title('WT')
-yyaxis right
-set(gca,'YTick',linspace(0,1,8),'YTickLabel',...
-    round(g1thetaindex_sorted(round(linspace(1,size(g1autocorrPopvec,1),8)),1)',2))
-ylabel('Theta Index')
-set(gca,'XMinorTick','on','YMinorTick','off',...
-    'LineWidth',1,'box','off','XTick',linspace(1,101,2),'XTickLabel',...
-    [-500 500],'FontSize',20,'FontWeight','bold','TickLength',[0,0])
+%Get first quarter index for Controls
+[~,pdIdx]=max(tuning1.S1,[],2);
+[~,ATNidx]=sort(pdIdx);
+ATN_sorted=tuning1.S1(ATNidx,:);
+ATN_sorted_2=tuning1.S2(ATNidx,:);
+ATN_sorted_3=tuning1.S3(ATNidx,:);
+ATN_sorted_4=tuning1.S4(ATNidx,:);
 
-subplot(1,2,2);
-imagesc(g2autocorrPopvec);
-box off;axis xy
-ylabel('Cells')
-xlabel('Lag (ms)')
-title('TgF344-AD')
-yyaxis right
-set(gca,'YTick',linspace(0,1,8),'YTickLabel',...
-    round(g2thetaindex_sorted(round(linspace(1,size(g2autocorrPopvec,1),8)),1)',2))
-ylabel('Theta Index')
-set(gca,'XMinorTick','on','YMinorTick','off',...
-    'LineWidth',1,'box','off','XTick',linspace(1,101,2),'XTickLabel',...
-    [-500 500],'FontSize',20,'FontWeight','bold','TickLength',[0,0])
+
+
+%Get first quarter index for Transgenics
+[~,pdIdx]=max(tuning2.S1,[],2);
+[~,ATNidx]=sort(pdIdx);
+ATN_sorted2=tuning2.S1(ATNidx,:);
+ATN_sorted2_2=tuning2.S2(ATNidx,:);
+ATN_sorted2_3=tuning2.S3(ATNidx,:);
+ATN_sorted2_4=tuning2.S4(ATNidx,:);
+
+vars=fieldnames(tuning1);
+for i=1:size(vars,1)
+    temp=tuning1.(vars{i});
+    for ii=1:size(temp,2)
+    cells(ii,:,i)=rescale(temp,ii,ii+1)
+    end
+end
+
+
+
+
+%Create PopVec Figure
+fig=figure;
+fig.Color=[1 1 1];
+subaxis(2,4,1)
+imagesc(ATN_sorted);axis off
+title('Session 1')
+subaxis(2,4,2)
+imagesc(ATN_sorted_2);axis off
+title('Session 2')
+subaxis(2,4,3)
+imagesc(ATN_sorted_3);axis off
+title('Session 3 - Rotated')
+subaxis(2,4,4)
+imagesc(ATN_sorted_4);axis off
+title('Session 4')
+
+subaxis(2,4,5)
+imagesc(ATN_sorted2);axis off
+subaxis(2,4,6)
+imagesc(ATN_sorted2_2);axis off
+subaxis(2,4,7)
+imagesc(ATN_sorted2_3);axis off
+subaxis(2,4,8)
+imagesc(ATN_sorted2_4); axis off
+
+y=[group1HD(:,9,1) group1HD(:,9,2) group1HD(:,9,3) group1HD(:,9,4)]; x=(1:size(y,2));
+SEM=nanstd(y)/sqrt(size(y,1));
+hold on; 
+shadedErrorBar(x,nanmean(y),SEM,'-k',1); 
+
+y=[group2HD(:,9,1) group2HD(:,9,2) group2HD(:,9,3) group2HD(:,9,4)]; x=(1:size(y,2));
+SEM=nanstd(y)/sqrt(size(y,1));
+shadedErrorBar(x,nanmean(y),SEM,'-r',1); 
+% ylim([.3 1]);
+
+data=[group1HD(:,9,1); group1HD(:,9,2); group1HD(:,9,3); group1HD(:,9,4); nan(1,1)];
+distIdx=[ones(size(group1HD,1),1); ones(size(group1HD,1),1); ones(size(group1HD,1),1);...
+    ones(size(group1HD,1),1); 2];
+catIdx=[ones(size(group1HD,1),1); ones(size(group1HD,1),1).*2; ones(size(group1HD,1),1).*3;...
+    ones(size(group1HD,1),1).*4;1];
+
+plotSpread(data,'categoryIdx',distIdx,'distributionIdx',catIdx,...
+    'categoryMarkers',{'o','o'},'categoryColors',{[.25 .25 .25],'k'})
+
+data=[group2HD(:,9,1); group2HD(:,9,2); group2HD(:,9,3); group2HD(:,9,4);nan(1,1)];
+distIdx=[ones(size(group2HD,1),1);ones(size(group2HD,1),1) ...
+    ;ones(size(group2HD,1),1);ones(size(group2HD,1),1);2];
+catIdx=[ones(size(group2HD,1),1);ones(size(group2HD,1),1).*2 ...
+    ;ones(size(group2HD,1),1).*3;ones(size(group2HD,1),1).*4;1];
+
+plotSpread(data,'categoryIdx',distIdx,'distributionIdx',catIdx,...
+    'categoryMarkers',{'o','o'},'categoryColors',{'r','k'})
+
+
+%All Data - R length Beeswarm with clusters that have min 100 spikes
+data=[group1(group1(:,39)>100,9,1); group1(group1(:,39)>100,9,2);...
+    group1(group1(:,39)>100,9,3); group1(group1(:,39)>100,9,4); nan(1,1)];
+distIdx=[ones(size(group1(group1(:,39)>100,9,1),1),1); ...
+    ones(size(group1(group1(:,39)>100,9,1),1),1); ...
+    ones(size(group1(group1(:,39)>100,9,1),1),1);...
+    ones(size(group1(group1(:,39)>100,9,1),1),1); 2];
+catIdx=[ones(size(group1(group1(:,39)>100,9,1),1),1); ...
+    ones(size(group1(group1(:,39)>100,9,1),1),1).*2; ...
+    ones(size(group1(group1(:,39)>100,9,1),1),1).*3;...
+    ones(size(group1(group1(:,39)>100,9,1),1),1).*4;1];
+
+plotSpread(data,'categoryIdx',distIdx,'distributionIdx',catIdx,...
+    'categoryMarkers',{'o','o'},'categoryColors',{[.25 .25 .25],'k'})
+
+data=[group2(group2(:,39)>100,9,1); group2(group2(:,39)>100,9,2);...
+    group2(group2(:,39)>100,9,3); group2(group2(:,39)>100,9,4);nan(1,1)];
+distIdx=[ones(size(group2(group2(:,39)>100,9,1),1),1);...
+    ones(size(group2(group2(:,39)>100,9,1),1),1) ...
+    ;ones(size(group2(group2(:,39)>100,9,1),1),1);...
+    ones(size(group2(group2(:,39)>100,9,1),1),1);2];
+catIdx=[ones(size(group2(group2(:,39)>100,9,1),1),1);...
+    ones(size(group2(group2(:,39)>100,9,1),1),1).*2 ...
+    ;ones(size(group2(group2(:,39)>100,9,1),1),1).*3;...
+    ones(size(group2(group2(:,39)>100,9,1),1),1).*4;1];
+
+plotSpread(data,'categoryIdx',distIdx,'distributionIdx',catIdx,...
+    'categoryMarkers',{'o','o'},'categoryColors',{'r','k'})
+
+fig=figure;
+figure.Color=[1 1 1];
+subaxis(1,4,1)
+[h1,h2]=CoolHistogram(group1(group1(:,39)>100,9,1),group2(group2(:,39)>100,9,1),10,'mean Vector Length')
+
+subaxis(1,4,2)
+[h1,h2]=CoolHistogram(group1(group1(:,39)>100,9,2),group2(group2(:,39)>100,9,2),10,'mean Vector Length')
+
+subaxis(1,4,3)
+[h1,h2]=CoolHistogram(group1(group1(:,39)>100,9,3),group2(group2(:,39)>100,9,3),10,'mean Vector Length')
+
+subaxis(1,4,4)
+[h1,h2]=CoolHistogram(group1(group1(:,39)>100,9,4),group2(group2(:,39)>100,9,4),10,'mean Vector Length')
 
 %% ___________________________LOCAL FUNCTION BELOW_________________________
 function [group,groupid]=placefieldfilter(group,groupid,varnames)
@@ -200,7 +193,7 @@ function [group,groupid]=placefieldfilter(group,groupid,varnames)
 %     sum(group(:,contains(varnames,'nSpikes'),1)>=100,3)>0;
 
 %IDX place cells
-idx=sum(group(:,contains(varnames,'PeakRate'),1)>=1,3)>0 &...
+idx=sum(group(:,contains(varnames,'PeakRate'),1)>=2,3)>0 &...
     sum(group(:,contains(varnames,'FieldWidth'),1)>=8,3)>0 &...
     sum(group(:,contains(varnames,'FieldWidth'),1)<=40,3)>0 & ...
     sum(group(:,contains(varnames,'InformationContent'),1)>=.4,3)>0 & ...
@@ -213,12 +206,13 @@ end
 
 %%
 function [group,groupid]=headdirectionfilter(group,groupid,varnames)
-% 1) Minimum peak firing rate of 2 Hz, 
+% 1) Minimum peak firing rate of 1 Hz, 
 % 2) Maximum field width of 8 cm,  
 % 3) at least 100 spikes
-idx=sum(group(:,contains(varnames,'PeakRate'),1)>=2,3)>0 &...
-    sum(group(:,contains(varnames,'mean_vector_length'),1)>=.5,3)>0 &...
-    sum(group(:,contains(varnames,'nSpikes'),1)>=100,3)>0;
+idx=sum(group(:,contains(varnames,'PeakRate'),1)>=1,3)>0 &...
+    sum(group(:,contains(varnames,'mean_vector_length'),1)>=.3,3)>0 &...
+    sum(group(:,contains(varnames,'nSpikes'),1)>=100,3)>0; % & ...
+    %sum(group(:,contains(varnames,'Direct_infoContent'),1)>=.3,3)>0;
 
 groupid=groupid(idx,:);
 group=group(idx,:,:);
@@ -253,29 +247,21 @@ function visualizecells(groupid,group)
 cd('F:\ClarkP30_Recordings\ProcessedData')
 for i=1:length(groupid)
     data=load(groupid{i,1});
-%     close all
+    
     p=postprocessFigures(data,{groupid{i,2},str2double(groupid(i,3))});
     set(gcf, 'Position', get(0, 'Screensize'));
-%     pause(3)
-
-
+    
     print(gcf,'-dpng', '-r300',...
         ['d:\Users\BClarkLab\Desktop\Laura Temp\PlaceCells\',group,...
         filesep,groupid{i,1},groupid{i,2},groupid{i,3},'.png'])
     close all
     
-
-
-%     saveas(gcf,['D:\Projects\PAE_PlaceCell',filesep,'test.emf'])
-%         p=postprocessFigures2(data,{groupid{i,2},str2double(groupid(i,3))});
-
-% print('-dmeta',['D:\Projects\PAE_PlaceCell',filesep,'test.emf'])
 end
 end
 
 
 function autoCorrMat=popVecAutocorr(groupid)
-cd('D:\ClarkP30_Recordings\ProcessedData')
+cd('F:\ClarkP30_Recordings\ProcessedData')
 autoCorrMat=[];
 for i=1:length(groupid)
     data=load(groupid{i,1},'thetaautocorr','spikesID');
@@ -291,7 +277,7 @@ end
 end
 
 function tuning=get_tuning(groupid)
-cd('D:\ClarkP30_Recordings\ProcessedData')
+cd('F:\ClarkP30_Recordings\ProcessedData')
 
 sessions={'S1','S2','S3','S4'};
 for i=1:length(groupid)
