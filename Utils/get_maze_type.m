918b664728e944c3132eead2331171f20df181de
Ryan <ryanh412@gmail.com>
2019-04-29 16:19:45 -0600

    forced to error out when no maze type was detected

--- Utils/get_maze_type.m
+++ Utils/get_maze_type.m
@@ -7,11 +7,15 @@ path_parts=strsplit(data.session_path,filesep);
 path_parts(end-2:end)=[];
 metadata_file=fullfile(strjoin(path_parts,filesep),'AnimalMetadata',[data.rat,'_metadata.mat']);
 if exist(metadata_file,'file')
-load(metadata_file,'AnimalMetadata');
-sessions=fieldnames(AnimalMetadata.RecordingLogs);
-mtypes=strsplit(AnimalMetadata.RecordingLogs.(sessions{ismember(sessions,data.sessionID)}).MazeTypes,',');
-
-for mt=1:size(data.events,2)
-    mazetype{mt}=mtypes{mt};
-end
+    load(metadata_file,'AnimalMetadata');
+    sessions=fieldnames(AnimalMetadata.RecordingLogs);
+    mtypes=strsplit(AnimalMetadata.RecordingLogs.(sessions{ismember(sessions,data.sessionID)}).MazeTypes,',');
+    
+    if isempty(mtypes{1})
+        return % will result in an error
+    end
+    
+    for mt=1:size(data.events,2)
+        mazetype{mt}=mtypes{mt};
+    end
 end
