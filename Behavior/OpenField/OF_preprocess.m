4160a0743f7af01fce684deb61606f9bc598b9b8
Laura Berkowitz <lberkowitz@unm.edu>
2019-06-19 17:25:39 -0600

    updated for deeplabcut xy coords

--- Behavior/OpenField/OF_preprocess.m
+++ Behavior/OpenField/OF_preprocess.m
@@ -1,17 +1,17 @@
 %OF_preprocess MGP & LB March 2019; updated by LB May 2019
 % This script will preprocess raw xy coordinates from open field
-% experiments. 
-%Input assumptions: 
+% experiments.
+%Input assumptions:
 %       A. Data are stored in a table named 'params' organized with the following columns:
 %           1. PathName (string variable, path to video data)
 %           2. VideoName (string variable - name of video file)
 %           3. dia (diameter of open field used in experiment)
 %           4 - 7. Xmax, Xmin, Ymax, Ymin (respectively). boundardy of open
-%           field used to transform coordinates. 
+%           field used to transform coordinates.
 %           8. rawcoords - nested cell array of Anymaze output.
 %           9. structured cell array of xy coordinates for boundary of cue
-%           used. 
-%       B. params can be created by importing anymaze csv files. 
+%           used.
+%       B. params can be created by importing anymaze csv files.
 %
 %Pipeline Description:
 %
@@ -24,58 +24,74 @@
 s=what('ephys_tools');
 cd(s.path)
 
-%Initialize Paths 
-addpath(fullfile('Behavior','OpenField','MeasureFunctions'),...
-    fullfile('Behavior','OpenField'),...
-    fullfile('Behavior'),...
-    fullfile('Utils'),...
-    fullfile('Visualize'),...
-    fullfile('external_packages','chronux_2_11'),...
+%Initialize Paths
+addpath(fullfile('external_packages','chronux_2_11'),...
     fullfile('external_packages','Colormaps'))
 
 cd('D:\Users\BClarkLab\Google Drive (lberkowitz@unm.edu)\Manuscripts\In Progress\TgF344-AD_OF\Data')
-load('params_V11.mat') %%loads table containing the raw coords and xMax/Min and yMax/Min
+load('params_V14.mat') %%loads table containing the raw coords and xMax/Min and yMax/Min
 clearvars -except params
 
 fr=30; %frame rate
-binsize=3; %to use for HB detection
-upFactor=15; %used in segmentimage - this upscales the heatmap to get a smooth boundary for home base. 
 
-%Initialze table for measures 
+%Initialze table for measures
 params.HBcoords{1}=[];
 params.dwellQuad{1}=[];
-params.transcoords{1}=[];%initialize column in table for transformed xy coordinates
+params.noseCM{1}=[];
+params.headCM{1}=[];
+params.backCM{1}=[];
+params.buttCM{1}=[];%initialize column in table for transformed xy coordinates
 params.HBcenter{1}=[];
 params.circuity{1}=[];
 params.proximity{1}=[];
-params.transcue{1}=[];
+params.cueCM{1}=[];
+params.hbOcc{1}=[];
+params.hbVel{1}=[];
+params.hbOcc{1}=[];
+params.hbVel{1}=[];
+params.hbXYidx{1}=[];
+params.entries{1}=[];
+params.HBclass{1}=[];
+params.HBBound{1}=[];
+params.slowInHB{1}=[];
 
 %% SMOOTH & TRANSFORM COORDINATES
 
-%Fixing tracking errors with FixPos and transforming coordinates into cm,
-% centered at 0,0
-for i = 1:size(params.PathName,1)-1 %%size gives 2 values, # of rows and columns, by subtracting 1 we keep it on the interval 1:296
-    params.ts{i}=linspace(0,size(params.rawcoords{i}(:,1),1)/fr, size(params.rawcoords{i}(:,1),1))'; %creates timestamps for Fixing tracking errors
-    [trackData(:,1),trackData(:,2)] = FixPos(params.rawcoords{i}(:,1),params.rawcoords{i}(:,2),params.ts{i}); %smooths & reduces error from tracking
-    [params.transcoords{i}(:,1),params.transcoords{i}(:,2)] = transformCoordinates(params.dia(i),params.Xmax(i),params.Xmin(i),params.Ymax(i),params.Ymin(i),trackData(:,1),trackData(:,2)); %Converts the coordinates into CM
-    params.transcoords{i,1}(~any(~isnan(params.transcoords{i,1}), 2),:)=[];
+%transforming coordinates into cm, centered at 0,0
+for i = 1:size(params.subID,1)
+    
+    [params.noseCM{i}(:,1),params.noseCM{i}(:,2)] = transformCoordinates...
+        (params.dia{i},params.Xmax{i},params.Xmin{i},params.Ymax{i},params.Ymin{i},...
+        params.nose{i}(:,1),params.nose{i}(:,2)); %Converts the coordinates into CM
+    
+    [params.headCM{i}(:,1),params.headCM{i}(:,2)] = transformCoordinates...
+        (params.dia{i},params.Xmax{i},params.Xmin{i},params.Ymax{i},params.Ymin{i},...
+        params.head{i}(:,1),params.head{i}(:,2)); %Converts the coordinates into CM
+    
+    [params.backCM{i}(:,1),params.backCM{i}(:,2)] = transformCoordinates...
+        (params.dia{i},params.Xmax{i},params.Xmin{i},params.Ymax{i},params.Ymin{i},...
+        params.back{i}(:,1),params.back{i}(:,2)); %Converts the coordinates into CM
+    
+    [params.buttCM{i}(:,1),params.buttCM{i}(:,2)] = transformCoordinates...
+        (params.dia{i},params.Xmax{i},params.Xmin{i},params.Ymax{i},params.Ymin{i},...
+        params.butt{i}(:,1),params.butt{i}(:,2)); %Converts the coordinates into CM
+    
     clear trackData
 end
 
 %%Transform Cue Coords
-for j=1:size(params.PathName,1)
-
-    fields = fieldnames(params.cue{j});
-    it=1;
-    for i = 1:2:(size(fields,1)) %%size gives 2 values, # of rows and columns, by subtracting 1 we keep it on the interval 1:296
-        [tempCueX(it,1),tempCueY(it,1)] =...
-            transformCoordinates(params.dia(j),params.Xmax(j),params.Xmin(j),...
-            params.Ymax(j),params.Ymin(j),params.cue{j}.(fields{i}){:},params.cue{j}.(fields{i+1}){:}); %Converts the coordinates into CM
-        it=it+1;
-    end
-    params.transcue{j}(:,1)=tempCueX;
-    params.transcue{j}(:,2)=tempCueY;
+for j=1:size(params.subID,1)
     
-    clear tempCueX tempCueY
+    if isempty(params.cueCoords{j})
+        params.cueCM{j}(:,1)=NaN;
+        params.cueCM{j}(:,2)=NaN;
+        continue
+    end
+  
+    [params.cueCM{j}(:,1),params.cueCM{j}(:,2)] =...
+        transformCoordinates(params.dia{j},params.Xmax{j},params.Xmin{j},...
+        params.Ymax{j},params.Ymin{j},params.cueCoords{j}(:,1),params.cueCoords{j}(:,2)); %Converts the coordinates into CM
 end
+
+
 
