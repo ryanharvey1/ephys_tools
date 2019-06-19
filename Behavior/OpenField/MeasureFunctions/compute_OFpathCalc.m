fec4d20acd4765ab4fd46276841e7a6315b788ef
Laura Berkowitz <lberkowitz@unm.edu>
2019-06-19 17:23:19 -0600

    changed PL calculation to only include distances when rat is moving greater than 3cm/s

--- Behavior/OpenField/MeasureFunctions/compute_OFpathCalc.m
+++ Behavior/OpenField/MeasureFunctions/compute_OFpathCalc.m
@@ -5,9 +5,11 @@ x_smooth(isnan(x_smooth))=0;
 y_smooth(isnan(y_smooth))=0;
 sqrXDiff=(diff(x_smooth)).^2;
 sqrYDiff=(diff(y_smooth)).^2;
-pathDist=sqrt(sqrXDiff + sqrYDiff);                                         %distance formula
-pathIV=(pathDist)*fr; % .10 = a frame rate of 10 hz                         %instanteous velocity
-PathLength=sum(pathDist);                                                   %Total Path length
+pathDist=sqrt(sqrXDiff + sqrYDiff);%distance formula
+IV=(pathDist)*fr; % 10 = a frame rate of 10 hz      %instanteous velocity
+pathIV=smoothdata(IV,'movmedian',fr*.8);
+
+PathLength=sum(pathDist(pathIV>=3,1));%Total Path length for points greater than 3cm/s
 
 end
 
