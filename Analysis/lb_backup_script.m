3b02e12822ab3d88fe455ed2090d17ef18d05046
Laura Berkowitz <lberkowitz@unm.edu>
2019-09-15 13:28:04 -0600

    added ATNppc01 backup

--- Analysis/lb_backup_script.m
+++ Analysis/lb_backup_script.m
@@ -3,3 +3,8 @@
 % Clark_P30 (LB and ATN projects)
 backup({'F:\ClarkP30_Recordings'},'Z:\Laura_Berkowitz\Projects')
 
+%% ATNppc
+% push to server
+backup({'F:\Projects\ATNppc'},'Z:\ryan_harvey')
+% pull from server
+backup({'Z:\ryan_harvey\Projects\ATNppc'},'F:','ignore_path','ryan_harvey')
