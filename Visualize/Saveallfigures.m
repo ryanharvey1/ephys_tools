ed2ea6f1840b519a33a97937dc5cfb2a7f8ef6f6
Ryan <ryanh412@gmail.com>
2019-10-13 14:37:38 -0600

    more general

--- Visualize/Saveallfigures.m
+++ Visualize/Saveallfigures.m
@@ -1,34 +1,42 @@
 % Saveallfigures
 close all
-cd D:\Projects\PAE_PlaceCell\ProcessedData
-processedpath=strsplit(data.session_path,filesep);
-processedpath(end-2:end)=[];
-cd(fullfile(strjoin(processedpath,filesep),'Figures'))
-
-sessions=dir('*.mat');
+% cd F:\Projects\PAE_PlaceCell\ProcessedData
+cd ..
+processedpath = pwd;
+% processedpath=strsplit(data.session_path,filesep);
+% processedpath(end-2:end)=[];
+% cd(fullfile(strjoin(processedpath,filesep),'Figures'))
+cd(fullfile(pwd,'ProcessedData'))
+sessions=dir(fullfile(processedpath,'ProcessedData','*.mat'));
 sessions={sessions.name};
 
 for i=1:length(sessions)
-data=load(sessions{i}); 
-p=postprocessFigures(data);
+    sess_id = extractBetween(sessions{i},'_','.mat');
 
-fig = get(groot,'CurrentFigure');
-
-while ~isempty(ishandle(fig) & strcmp(get(fig, 'type'), 'figure'))
-
-    id=strsplit(fig.Name,'  ');
-    
-    if exist([fullfile(strjoin(processedpath,filesep),'Figures'),filesep,id{1},filesep,id{2}],'dir')==0
-        mkdir([fullfile(strjoin(processedpath,filesep),'Figures'),filesep,id{1},filesep,id{2}]);
+    if exist(fullfile(processedpath,'Figures',extractBefore(sessions{i},'_'),sess_id{:}),'dir')
+        continue
     end
     
-    set(fig, 'Position', get(0, 'Screensize'));
-    if length(id)==3
-        print(fig,'-dpng', '-r300',[fullfile(strjoin(processedpath,filesep),'Figures'),filesep,id{1},filesep,id{2},filesep,erase(id{3},':'),'.png'])
-    elseif length(id)==2
-        print(fig,'-dpng', '-r300',[fullfile(strjoin(processedpath,filesep),'Figures'),filesep,id{1},filesep,id{2},filesep,'raster.png'])
-    end
-    close
+    data=load(sessions{i});
+    postprocessFigures.main(data,'colorcode','HD');
+    
     fig = get(groot,'CurrentFigure');
-end
+    
+    while ~isempty(ishandle(fig) & strcmp(get(fig, 'type'), 'figure'))
+        
+        id=strsplit(fig.Name,'  ');
+        
+        if exist([fullfile(processedpath,'Figures'),filesep,id{1},filesep,id{2}],'dir')==0
+            mkdir([fullfile(processedpath,'Figures'),filesep,id{1},filesep,id{2}]);
+        end
+        
+        set(fig, 'Position', get(0, 'Screensize'));
+        if length(id)==3
+            print(fig,'-dpng', '-r300',[fullfile(processedpath,'Figures'),filesep,id{1},filesep,id{2},filesep,erase(id{3},':'),'.png'])
+        elseif length(id)==2
+            print(fig,'-dpng', '-r300',[fullfile(processedpath,'Figures'),filesep,id{1},filesep,id{2},filesep,'raster.png'])
+        end
+        close
+        fig = get(groot,'CurrentFigure');
+    end
 end
