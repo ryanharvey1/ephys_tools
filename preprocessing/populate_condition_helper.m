04dca7ed261f15db3b4182246fc773ab7939e21c
Ryan <ryanh412@gmail.com>
2019-10-24 14:26:55 -0600

    made a few changes in the right direction towards a general condition helper, but more work is needed

--- preprocessing/populate_condition_helper.m
+++ preprocessing/populate_condition_helper.m
@@ -1,21 +1,32 @@
 % populate_mazetype_helper
-cd F:\ClarkP30_Recordings\AnimalMetaData
+% cd F:\ClarkP30_Recordings\AnimalMetaData
 animals=dir('*.mat');
 animals={animals.name};
 Condition=[];
+
+conditions=get_conditions;
 
 for a=1:length(animals)
     load(animals{a})
     sessions=fieldnames(AnimalMetadata.RecordingLogs);
     s=1;
-    for i=size(MazeTypes,1)+1:length(sessions)+size(MazeTypes,1)
+    for i=size(Condition,1)+1:length(sessions)+size(Condition,1)
         Condition{i,1}=animals{a};
         Condition{i,2}=[sessions{s}];
         Condition{i,3}=[AnimalMetadata.RecordingLogs.(sessions{s}).MazeTypes];
+        Condition{i,4}=[AnimalMetadata.RecordingLogs.(sessions{s}).Notes];
         s=s+1;
     end
 end
-Condition
+
+
+% Condition_table = table(Condition(:,1),Condition(:,2),Condition(:,3),'VariableNames',{'animal','session','maze'});
+% for i=1:length(conditions)
+%     Condition_table.(conditions{i})(1:size(Condition,1),1)=NaN(size(Condition,1),1);
+% end
+
+
+
 %% manually fix all maze type errors and fill everything in before continuing to next section
 
 %% Save everything 
@@ -24,7 +35,7 @@ for a=1:length(animals)
     load(animals{a})
     sessions=fieldnames(AnimalMetadata.RecordingLogs);
     for i=1:length(sessions)
-        AnimalMetadata.RecordingLogs.(sessions{i}).ConditionTypes=Condition{s,3};
+        AnimalMetadata.RecordingLogs.(sessions{i}).ConditionTypes=Condition{s,4};
         s=s+1;
     end
     metapath=fullfile(pwd,[AnimalMetadata.AnimalName,'_metadata.mat']);
