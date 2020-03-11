87fd10f16eba1d634c0650c6d0ee9bd78128542a
Ryan <ryanh412@gmail.com>
2020-03-11 16:15:21 -0600

    ripple figures and classification

---
+++ LFP/manual_ripple_classification.m
@@ -0,0 +1,320 @@
+% manual_ripple_classification
+basepath = 'F:\Projects\PAE_PlaceCell';
+
+if exist(fullfile(basepath,'SWR_workspace.mat'),'file')
+    load(fullfile(basepath,'SWR_workspace.mat'),'SWR');
+else
+    sessions = dir(fullfile(basepath,'SWR','*.mat'));
+    maps = [];
+    maps_filtered = [];
+    duration = [];
+    peakFrequency = [];
+    peakAmplitude = [];
+    session_id = [];
+    ripple_id = [];
+    for s = 1:length(sessions)
+        disp(fullfile(sessions(s).folder,sessions(s).name))
+        ripple_info = load(fullfile(sessions(s).folder,sessions(s).name));
+        
+        maps = [maps;ripple_info.maps.unfiltered_ripples];
+        maps_filtered = [maps_filtered;ripple_info.maps.ripples];
+        
+        duration = [duration;ripple_info.ripple_data.duration];
+        peakFrequency = [peakFrequency;ripple_info.ripple_data.peakFrequency];
+        peakAmplitude = [peakAmplitude;ripple_info.ripple_data.peakAmplitude];
+        
+        session_id = [session_id; repmat({ripple_info.ripples.detectorinfo.ProcessedDatafile},...
+            size(ripple_info.maps.ripples,1),1)];
+        
+        ripple_id = [ripple_id;[1:size(ripple_info.maps.ripples,1)]'];
+    end
+    SWR.maps = maps;
+    SWR.maps_zscore = zscore(SWR.maps,[],2);
+    SWR.maps_filtered = maps_filtered;
+    
+    SWR.data.duration = duration;
+    SWR.data.peakFrequency = peakFrequency;
+    SWR.data.peakAmplitude = peakAmplitude;
+    SWR.data.session_id = session_id;
+    SWR.data.ripple_id = ripple_id;
+    SWR.data.result = zeros(size(SWR.maps,1),1);
+    SWR.data.labeled = zeros(size(SWR.maps,1),1);
+    
+    SWR.data = struct2table(SWR.data);
+    
+    save(fullfile(basepath,'SWR_workspace.mat'),'SWR');
+end
+
+move_fig(basepath)
+
+%%
+
+
+%%
+
+% figure;
+% 
+% [coeff,score,latent,tsquared,explained,mu] = pca(SWR.maps_zscore);
+% subplot(3,2,5)
+% h = scatter(score(:,1),score(:,2),3,zeros(length(score(:,2)),3)+.5,'Filled');
+% h.SizeData = zeros(length(score(:,2)),1) + 5;
+% h.MarkerFaceAlpha = .7;
+% axis tight
+% hold on
+% 
+% for i = 1:length(SWR.maps)
+%     
+%     h.CData = zeros(length(score(:,2)),3);
+%     
+%     sgtitle(['duration: ',num2str(SWR.duration(i)),'s ',...
+%         'freq: ',num2str(round(SWR.peakFrequency(i))),'Hz ',...
+%         num2str(i),' of ',num2str(size(SWR.maps,1))],...
+%         'color','r')
+%     
+%     subplot(3,2,5)
+%     h2 = scatter(score(i,1),score(i,2),10,[1 0 0],'Filled');
+%     
+%     
+%     subplot(3,2,1)
+%     plot(1:151,SWR.maps_zscore(i,:),'Color',[.7,.7,.7])
+%     
+%     subplot(3,2,2)
+%     plot(1:151,SWR.maps_filtered(i,:),'Color',[.7,.7,.7])
+%     darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+%     
+%     SWR.data.result(i) = input('ripple ? ');
+%     SWR.data.labeled(i) = 1;
+%     delete(h2)
+%     
+%     if SWR.result(i)
+%         subplot(3,2,3)
+%         plot(1:151,SWR.maps_zscore(i,:),...
+%             'Color',[rand(1, 1),rand(1, 1),rand(1, 1),0.1])
+%         title('good')
+%         hold on
+%     end
+%     if ~SWR.result(i)
+%         subplot(3,2,4)
+%         plot(1:151,SWR.maps_zscore(i,:),...
+%             'Color',[rand(1, 1),rand(1, 1),rand(1, 1),0.1])
+%         title('bad')
+%         hold on
+%     end
+%     
+%     
+%     darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+% end
+
+
+
+% maps_zscore = zscore(maps,[],2);
+% 
+% 
+% [reduction, umap, clusterIdentifiers] = run_umap(maps_zscore);
+% clusters = unique(clusterIdentifiers);
+% p = ceil(sqrt(length(clusters)));
+% figure;
+% for i = double(clusters)
+%     subplot(p,p,i+1)
+%     plot(1:151,maps(clusterIdentifiers == i,:),'Color',[.7,.7,.7,0.1])
+%     box off
+%     axis off
+% end
+% darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+% 
+% figure;
+% for i = double(clusters)
+%     plot(1:151,maps_zscore(clusterIdentifiers == i,:),...
+%         'Color',[rand(1, 1),rand(1, 1),rand(1, 1),0.1])
+%     hold on
+% end
+% darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+% axis off
+% 
+% 
+% idx = kmeans(reduction,3,'Distance','cityblock');
+% figure;
+% scatter(reduction(idx==1,1),reduction(idx==1,2),'r','Filled')
+% hold on
+% scatter(reduction(idx==2,1),reduction(idx==2,2),'g','Filled')
+% scatter(reduction(idx==3,1),reduction(idx==3,2),'b','Filled')
+% axis image
+% 
+% 
+% 
+% 
+% figure;
+% scatter3(duration,peakFrequency,peakAmplitude,'k','Filled')
+% 
+% maps_filtered_zscore = zscore(maps_filtered,[],2);
+% 
+% 
+% maps_zscore = zscore(maps,[],2);
+% 
+% [coeff,score,latent,tsquared,explained,mu] = pca(maps_zscore);
+% [coeff,score,latent,tsquared,explained,mu] = pca(maps);
+% 
+% 
+% figure;
+% scatter3(score(:,1),score(:,2),score(:,3),'k','Filled')
+% axis image
+% 
+% idx = kmeans(score,2);
+% figure;
+% scatter3(score(:,1),score(:,2),score(:,3),'k','Filled')
+% hold on
+% scatter3(score(idx==1,1),score(idx==1,2),score(idx==1,3),'r','Filled')
+% axis image
+% 
+% figure
+% 
+% biplot(coeff(:,1:3),'scores',score(:,1:3));
+% axis image
+
+
+
+function [] = move_fig(basepath)
+% move figure with arrow keys.
+S.fh = figure('units','pixels',...
+    'menubar','none',...
+    'name','move_fig',...
+    'numbertitle','off',...
+    'keypressfcn',@fh_kpfcn);
+S.basepath = basepath;
+S.start_of_session = 1;
+S.i = 1;
+% S.tx = uicontrol('style','text',...
+%     'units','pixels',...
+%     'position',[60 120 80 20],...
+%     'fontweight','bold');
+guidata(S.fh,S)
+disp('Hit Enter')
+end
+function [S] = fh_kpfcn(H,E)
+% Figure keypressfcn
+S = guidata(H);
+P = get(S.fh,'position');
+
+i = S.i;
+
+if S.start_of_session
+    load(fullfile(S.basepath,'SWR_workspace.mat'),'SWR');
+    
+    i = find(SWR.data.labeled,1,'last');
+    if isempty(i)
+        i = 1;
+    end
+    S.SWR = SWR;
+else
+    SWR = S.SWR;
+end
+
+if ~S.start_of_session
+    switch E.Key
+        case 'rightarrow'
+            i = i + 1;
+        case 'leftarrow'
+            i = i - 1;
+        case 'uparrow'
+            SWR.data.result(i) = 1;
+            SWR.data.labeled(i) = 1;
+            i = i + 1;
+        case 'downarrow'
+            SWR.data.result(i) = 0;
+            SWR.data.labeled(i) = 1;
+            i = i + 1;
+        otherwise
+            save(fullfile(S.basepath,'SWR_workspace.mat'),'SWR');
+    end
+end
+disp([SWR.data.session_id{i},...
+    ' ripple ',num2str(SWR.data.ripple_id(i)),...
+    ' Labeled: ',num2str(SWR.data.labeled(i)),...
+    ' ripple: ',num2str(SWR.data.result(i))])
+
+if S.start_of_session
+    [coeff,score,latent,tsquared,explained,mu] = pca(SWR.maps_zscore);
+    subplot(3,2,5)
+    h = scatter(score(:,1),score(:,2),3,zeros(length(score(:,2)),3)+.5,'Filled');
+    h.SizeData = zeros(length(score(:,2)),1) + 5;
+    h.MarkerFaceAlpha = .7;
+    axis tight
+    hold on
+    h.CData = zeros(length(score(:,2)),3);
+    S.score = score;
+    xlabel('pc1')
+    ylabel('pc2')
+    
+    subplot(3,2,6)
+    h = scatter(SWR.data.duration,SWR.data.peakFrequency,3,...
+        zeros(length(score(:,2)),3)+.5,'Filled');
+    h.SizeData = zeros(length(score(:,2)),1) + 5;
+    h.MarkerFaceAlpha = .7;
+    axis tight
+    hold on
+    h.CData = zeros(length(score(:,2)),3);
+    xlabel('duration(s)')
+    ylabel('freq')
+end
+S.start_of_session = 0;
+
+sgtitle(['duration: ',num2str(SWR.data.duration(i)),'s ',...
+    'freq: ',num2str(round(SWR.data.peakFrequency(i))),'Hz ',...
+    num2str(i),' of ',num2str(size(SWR.maps,1))],...
+    'color','r')
+
+subplot(3,2,1)
+plot(1:151,SWR.maps_zscore(i,:),'Color',[.7,.7,.7])
+title('raw ripple')
+
+subplot(3,2,2)
+plot(1:151,SWR.maps_filtered(i,:),'Color',[.7,.7,.7])
+title('filtered ripple')
+
+subplot(3,2,5)
+scatter(S.score(i,1),S.score(i,2),10,[1,1,1],'Filled');
+
+subplot(3,2,6)
+scatter(SWR.data.duration(i),SWR.data.peakFrequency(i),10,[1,1,1],'Filled');
+
+darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+
+if i > 1
+    if SWR.data.result(i-1) && SWR.data.labeled(i-1)
+        subplot(3,2,3)
+        plot(1:151,SWR.maps_zscore(i-1,:),...
+            'Color',[rand(1, 1),rand(1, 1),rand(1, 1),0.1])
+        title('good','Color','g')
+        hold on
+        
+        subplot(3,2,5)
+        scatter(S.score(i-1,1),S.score(i-1,2),10,[0 1 0],'Filled');
+    end
+    if ~SWR.data.result(i-1) && SWR.data.labeled(i-1)
+        subplot(3,2,4)
+        plot(1:151,SWR.maps_zscore(i-1,:),...
+            'Color',[rand(1, 1),rand(1, 1),rand(1, 1),0.1])
+        title('bad','Color','r')
+        hold on
+        
+        subplot(3,2,5)
+        scatter(S.score(i-1,1),S.score(i-1,2),10,[1 0 0],'Filled');
+    end
+ 
+    if SWR.data.result(i-1) && SWR.data.labeled(i-1)
+        subplot(3,2,6)
+        scatter(SWR.data.duration(i-1),SWR.data.peakFrequency(i-1),10,[0 1 0],'Filled');
+    end
+    
+    if ~SWR.data.result(i-1) && SWR.data.labeled(i-1)
+        subplot(3,2,6)
+        scatter(SWR.data.duration(i-1),SWR.data.peakFrequency(i-1),10,[1 0 0],'Filled');
+    end
+end
+darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+
+S.i = i;
+S.SWR = SWR;
+guidata(S.fh,S)
+
+end
---
+++ LFP/ripple_figs.m
@@ -0,0 +1,137 @@
+classdef ripple_figs
+    methods(Static)
+        
+        function lfp_viewer(ripple_info,data)
+            
+            if ~exist('data','var')
+                disp('loading lfp ...')
+                data = load(ripple_info.ripples.detectorinfo.ProcessedDatafile,'lfp');
+            end
+            figure
+            trip = colormap(cool(size(data.lfp.signal,1)));
+            for i = 1:size(data.lfp.signal,1)
+                plot(data.lfp.ts,zscore(data.lfp.signal(i,:)) + i*2,'Color',trip(i,:))
+                hold on
+            end
+            EMGFromLFP = load(ripple_info.ripples.detectorinfo.detectionparms.EMGfilename);
+            plot(EMGFromLFP.timestamps,zscore(EMGFromLFP.data),'k')
+
+            for r = 1:size(ripple_info.ripples.filtered_ripple,2)
+                
+                plot([ripple_info.ripples.peaks(r),ripple_info.ripples.peaks(r)],...
+                    [min(ylim) max(ylim)],'r')
+                hold on
+                xbars = ripple_info.ripples.timestamps(r,:);
+                patch([xbars(1) xbars(1), xbars(2) xbars(2)],...
+                    [min(ylim) max(ylim) max(ylim) min(ylim)],...
+                    [1 0 0],'FaceAlpha',.3,'EdgeColor','none')
+            end
+            
+            ch_used = num2str(ripple_info.ripples.detectorinfo.detectionparms.channel_used);
+            noise = num2str(ripple_info.ripples.detectorinfo.detectionparms.noise_channel);  
+            title(['ch used: ',ch_used,' noise ch: ',noise,...
+                ', Black: EMG',', Red: ripple'])
+            
+            darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+        end
+        
+        function ripple_grid(ripple_info)
+            figure;
+            p = ceil(sqrt(size(ripple_info.ripples.unfiltered_ripple,2)));
+            colors = viridis(length(unique(ripple_info.ripples.peakNormedPower)));
+            for r = 1:size(ripple_info.ripples.filtered_ripple,2)
+                subplot(p,p,r)
+                plot(ripple_info.ripples.unfiltered_ripple{r},...
+                    'color',interp1(unique(ripple_info.ripples.peakNormedPower),...
+                    colors,ripple_info.ripples.peakNormedPower(r)))
+                hold on
+                box off
+                axis off
+            end
+            sgtitle([num2str(r),' Ripples on Channel ',...
+                num2str(ripple_info.ripples.detectorinfo.detectionparms.channel_used),...
+                ', Color code: power'],'Color','w')
+            darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+        end
+        
+        function ripple_per_channel(ripple_info,data)
+            
+            if ~exist('data','var')
+                disp('loading lfp ...')
+                data = load(ripple_info.ripples.detectorinfo.ProcessedDatafile,'lfp');
+            end
+            
+            n_ripples = size(ripple_info.ripples.timestamps,1);
+            colors = [rand(n_ripples,1),rand(n_ripples,1),rand(n_ripples,1)];
+            
+            duration = 1;
+            figure
+            for r = 1:n_ripples
+                idx = data.lfp.ts >= ripple_info.ripples.timestamps(r,1) &...
+                    data.lfp.ts <= ripple_info.ripples.timestamps(r,2);
+                plot(duration:duration+sum(idx)-1,...
+                    zscore(data.lfp.signal(:,idx),[],2)+[[1:size(data.lfp.signal,1)]*3]',...
+                    'Color',colors(r,:))
+                box off
+                hold on
+                duration = sum(idx) + duration;
+            end
+            title('Unfiltered ripples')
+            darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+            
+            % filtered plot
+            disp('filtering lfp ...')
+            for i =1:size(data.lfp.signal,1)
+                signal_filtered(i,:) = BandpassFilter(data.lfp.signal(i,:), 1000,...
+                    ripple_info.ripples.detectorinfo.detectionparms.passband);
+            end
+            duration = 1;
+            figure
+            for r = 1:n_ripples
+                idx = data.lfp.ts >= ripple_info.ripples.timestamps(r,1) &...
+                    data.lfp.ts <= ripple_info.ripples.timestamps(r,2);
+                plot(duration:duration+sum(idx)-1,...
+                    zscore(signal_filtered(:,idx),[],2)+[[1:size(signal_filtered,1)]*3]',...
+                    'Color',colors(r,:))
+                box off
+                hold on
+                duration = sum(idx) + duration;
+            end
+            title('Filtered ripples')
+            darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+        end
+        
+        function ripple_location(ripple_info,data)
+            
+            if ~exist('data','var')
+                disp('loading lfp ...')
+                data = load(ripple_info.ripples.detectorinfo.ProcessedDatafile,...
+                    'mazetypes','linear_track','events','frames');
+            end
+            figure;
+            for i = 1:size(data.mazetypes,2)
+                subplot(1,size(data.mazetypes,2),i)
+                if contains(data.mazetypes{i},"track",'IgnoreCase',true)
+                    frames = data.linear_track{1}.nonlinearFrames;
+                    plot(frames(:,2),frames(:,3),'Color',[.7 .7 .7])
+                    hold on
+                    ripple_frames = interp1(frames(:,1),frames,ripple_info.ripples.peaks);
+                    scatter(ripple_frames(:,2),ripple_frames(:,3),10,'r','Filled')
+                    axis image
+                else
+                    idx = data.frames(:,1) > data.events(1,i) &...
+                        data.frames(:,1) < data.events(2,i);
+                    frames = data.frames(idx,:);
+                    plot(frames(:,2),frames(:,3),'Color',[.7 .7 .7])
+                    hold on
+                    ripple_frames = interp1(frames(:,1),frames,ripple_info.ripples.peaks);
+                    scatter(ripple_frames(:,2),ripple_frames(:,3),10,'r','Filled')
+                    axis image
+                end
+                title(data.mazetypes{i})
+            end
+            sgtitle('Red dot = ripple','color','r')
+            darkBackground(gcf,[0.1 0.1 0.1],[0.7 0.7 0.7])
+        end
+    end
+end
