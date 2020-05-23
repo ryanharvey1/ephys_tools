f27fd60e481abe369d60728320ea5fac39746696
Ryan <ryanh412@gmail.com>
2020-05-23 09:22:42 -0600

    removed NSMA

--- external_packages/NSMA_Toolbox/Analyses/Reactivation_CPP-Saline/Analysis/code/MakeEpochTimesForSessionList.m
+++
@@ -1,8 +0,0 @@
-%% prints 3 formated strings to copy and paste into SessionList.m template
-%% needs the epoch start-end times in workspace
-%% e.g :  s.epochs.sleep1 = [14969140.37,20969140.37];
-
-
-disp(['s.epochs.sleep1 = [' num2str(ts_start_s1) ',' num2str(ts_end_s1) '];'])
-disp(['s.epochs.maze1  = [' num2str(ts_start_b1) ',' num2str(ts_end_b1) '];'])
-disp(['s.epochs.sleep2 = [' num2str(ts_start_s2) ',' num2str(ts_end_s2) '];'])
--- external_packages/NSMA_Toolbox/IO/ReadST.m
+++
@@ -1,10 +0,0 @@
-function ST = ReadST(fn)
-
-% ReadST  Reads an NSMA ST file
-%
-% ST = ReadST(fn)
-%
-% INPUTS:
-%   fn = .ST file% OUTPUTS:%   ST = tsd structure where data = nSpikes x nSamplesPerSpike x nTrodes%% Uses mex file ReadST (PL '99) to do the main read%% ADR 2000, version L1.0, last modified '00 by ADR
-% RELEASED as part of MClust 2.0% See standard disclaimer in Contents.m
-[t, wv] = ReadST(fn);ST = tsd(t, wv);
