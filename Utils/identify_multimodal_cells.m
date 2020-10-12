86e0c9066af40d437b509c2e07e3d0421e401473
Laura Berkowitz <lberkowitz@unm.edu>
2020-10-11 21:32:21 -0600

    removed circular uniform tests

--- Utils/identify_multimodal_cells.m
+++ Utils/identify_multimodal_cells.m
@@ -1,108 +1,150 @@
-% identify_multimodal_cells is intended to automatically classify and
-% measure cells that fire maximally at multiple head angles (within yaw
-% plane). 
+% identify_multimodal_cells is intended to classify and
+% measure cells that fire maximally at multiple head angles (within
+% horizontal plane).
 
-%Code is in the process of being developed. 
+%Code is in the process of being developed.
 
-%To be addressed: 
+%To be addressed:
 
 % 1. find and prevent reason for fitvmdist failures and create permanent
-% patch. 
+% patch.
 
 % 2. Kuiper test is too sensitive? Find more reliable test of circular
-% uniformity. 
+% uniformity.
 
-% 3. Find beter method of  hyperparameter optimization for fitvmdist, 
-% perhaps instead, create new approach using methods described in Olson et al., 2016. 
+% 3. Find beter method of  hyperparameter optimization for fitvmdist,
+% perhaps instead, create new approach using methods described in Olson et al., 2016.
 
-% 4. Create cross-validation method to verify model selection. 
+% 4. Create cross-validation method to verify model selection.
 
 %set parameters
 numDist = 4;
 bin_centers = 3:6:360-3;
+nfolds = 5;
+%Test data
+data = load('F:\ClarkP30_Recordings\ProcessedData\LB07_S20190429174041.mat');
 
-temp = load('F:\ClarkP30_Recordings\ProcessedData\LB07_S20190429174041.mat');
 
-
- for cell = 1:length(temp.Spikes)
-    
-    for ii=1:3
+% for cell = 1:length(temp.Spikes) % loop across cells
+%     
+%     for ii = 1:3 % loop across sessions
+%         
+        %Check spike quality
+        cell = 31; ii = 1;
         
-        %Check spike quality 
-        
-        num_spikes = size(temp.Spikes{cell},1);
-        tuning = temp.hdTuning{cell,ii};
+        num_spikes = size(data.Spikes{cell},1);
+        tuning = data.hdTuning{cell,ii};
         peak_FR = max(tuning);
         
+        % not including low firing cells
         if num_spikes < 100 || peak_FR < 2 || ii>4
             final_model(cell,ii) = NaN;
-            continue
+%             continue
         end
         
-        
         % get tuning curve to create samples
-        tuning = temp.hdTuning{cell,ii};
+        tuning = data.hdTuning{cell,ii};
         
         dir_prop = round(tuning*10); % use number to create tuning curve
         
-        samples_deg=[]; 
+        samples_deg=[];
         for d = 1:size(bin_centers,2)
             samples_deg = [samples_deg; repmat(bin_centers(1,d),dir_prop(1,d),1)];
         end
         
-        samples=wrapToPi(deg2rad(samples_deg));
+        samples = wrapToPi(deg2rad(samples_deg));
         
-        % create uniform circular distribution 
+        % create uniform circular distribution
         vmm = VonMisesMixture(1, 0, 0);  % Initialize model.
         uniform = vmm.random(size(samples,1));          % Draw n random samples to match data.
         
-        % Check if data is uniformally distributed
-         [pval, kpval, Kpval(ii,1)] = circ_kuipertest(uniform, samples, 100, 1);
+        % Create incidies for 10-fold cross validation
+        indices = crossvalind('Kfold',samples,nfolds);
         
         
-        if pval < .05 %&& pval > .05
-            fig=figure;
-            fig.Color=[1 1 1]
-            
-            % evaluate models with 1 through 4 components
-            for iii=1:numDist
-                
-                try 
-                fittedVmm= fitmvmdist(samples, iii, ...
-                    'MaxIter', 250); % Set maximum number of EM iterations to 300
-                
-                catch 
-                    final_model(cell,ii) = NaN;
-                    break
-                end
-                fit_measures(iii)=fittedVmm.logLikelihood;
-            
-                angles = linspace(-pi, pi, size(samples,1))';
-                likelihoodsFitted = fittedVmm.pdf(angles);
-                
-                subplot(1,numDist,iii)
-                postprocessFigures.plot_HD_tuning(temp,ii,cell)
-                hold on;
-                Polarplot=polar(angles, rescale(likelihoodsFitted,0,max(temp.hdTuning{cell,ii})));
-                set(Polarplot,'linewidth',2,'color','r');
-                title([num2str(fit_measures(iii)),' numcomponents=  ',num2str(fittedVmm.nComponents)])
-                
-                % initialize final_model
-                final_model(cell,ii) = 0;
-            end
-            
-            if isnan( final_model(cell,ii))
-                break
-            end
-            
-            winner = find([diff(fit_measures),0]./abs(fit_measures)>=.2)+1;
-            if isempty(winner)
-                winner = 1;
-            end
-            final_model(cell,ii) = winner(1);
+        for i = 1 : nfolds
+            test_samples = samples(indices == i,1);
+            train_samples = samples(indices ~= i,1);
+           
+            disp(['test set ',num2str(i),' train with all other sets'])
 
+            res_train{i,1} = runit(train_samples, numDist);
+            res_test{i,1} = runit(test_samples, numDist);
+            
         end
+        
+%         if isnan( final_model(cell,ii))
+% %             break
+%         end
+%         
+%     end
+% end
 
+
+function res = runit(samples, maxOrder)
+
+    %% model fit
+    % (may take a while)
+    for model_num = 1:maxOrder
+        do = 1;
+        while do
+            try
+                GMModel(model_num).res = fitmvmdist(samples,model_num);
+                do = 0;
+            catch
+                do = 1;
+            end
+        end
     end
- end
     
+    %% Calculate BIC
+    BIC = arrayfun(@(x) log(length(samples))*x.res.nComponents - 2*x.res.logLikelihood, GMModel);
+
+    
+     %% get parameters
+     for i = 1:maxOrder
+         mu{i} = arrayfun(@(x) x.mu, GMModel(i).res,'UniformOutput',0);
+         k{i} =  arrayfun(@(x) x.componentProportion, GMModel(i).res,'UniformOutput',0);
+     end
+     
+     %% package
+     res.mu = mu;
+     res.BIC = BIC;
+     res.k = k;
+     res.fits = GMModel;
+     
+end
+
+% %% BIC plot
+%     if ifPlot == 1
+%         figure();hold on
+%         plot(BIC)
+%         xlabel('Model Order')
+%         ylabel('BIC')
+%         [minVal, minArg] = min(BIC);
+%         plot(minArg,minVal,'ro','MarkerFaceColor','r')
+% 
+%     
+%         %% Plot model fits
+%         figure
+%         for i = 1:5
+%             axesHandle(i) = subplot(2,5,i);
+%             
+%             % tricsky subplotting polar plots
+%             polarAxesHandle(i) = polaraxes('Units',axesHandle(i).Units,'Position',axesHandle(i).Position);
+%             delete(axesHandle(i));
+%             
+%             % back to normal
+%             binEdges = linspace(-pi,pi,30);
+%             counts = histcounts(data,binEdges);
+%             counts = counts / sum(counts);
+%             polarhistogram(polarAxesHandle(i), 'BinEdges',binEdges, 'BinCounts', counts)
+% 
+%             hold on
+%             evalPts = -pi:0.01:pi; evalPts = evalPts(:);
+%             ft = pdf(GMModel(i).res,evalPts);
+%             ft = ft / 4;  % Honestly no clue why this works ... 
+%             polarplot(evalPts,ft,'LineWidth',3);
+%             title(['Model Order = ' num2str(i)])
+%         end
+%     end
