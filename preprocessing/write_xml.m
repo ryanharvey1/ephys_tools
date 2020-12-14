6edd703e053f8151b9c361cec66cd47b7a352f67
Laura Berkowitz <lberkowitz@unm.edu>
2020-12-14 11:37:41 -0700

    wrapper for buzcode write xml

---
+++ preprocessing/write_xml.m
@@ -0,0 +1,17 @@
+    % write xml
+    function write_xml(path,map,Fold,FNew)
+    % Wrapper for buzcode bz_MakeXMLFromProbeMaps with defaults
+    
+    defaults.NumberOfChannels = 1;
+    defaults.SampleRate = Fold;
+    defaults.BitsPerSample = 16;
+    defaults.VoltageRange = 20;
+    defaults.Amplification = 1000;
+    defaults.LfpSampleRate = FNew;
+    defaults.PointsPerWaveform = 64;
+    defaults.PeakPointInWaveform = 32;
+    defaults.FeaturesPerWave = 4;
+    [~,basename] = fileparts(path);
+    bz_MakeXMLFromProbeMaps({map},path,basename,1,defaults)
+    end
+    
