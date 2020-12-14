f8adf5d6f70f591f5f8a7f93b7e7dd4af1e8ccca
Laura Berkowitz <lberkowitz@unm.edu>
2020-12-14 11:37:04 -0700

    changed for clarkp30 recordings

--- io/re_write_lfp_files.m
+++ io/re_write_lfp_files.m
@@ -1,16 +1,26 @@
 % re-write all .lfp files to 1250hz
 
 % find all lfp files
-lfp_files = dir('F:\Projects\PAE_PlaceCell\data\**\*.lfp');
+% lfp_files = dir('F:\ClarkP30_Recordings\Data\**\*.lfp');
+lfp_files = dir('F:\ClarkP30_Recordings\Data\**\*.nvt'); % loop through all folders
 
 Fold = 32000;
 Fnew = 1250;
+
 WaitMessage = parfor_wait(length(lfp_files),'Waitbar',true);
 % loop through each file
 for i = 1:length(lfp_files)
+    [~,basename] = fileparts(lfp_files(i).folder);
     
     % load raw data
     lfpfile = locate_ncs(lfp_files(i).folder);
+    
+    % locate xml 
+    if isempty(dir([lfp_files(i).folder,filesep,'**\*.xml']))
+        probe_map = get_channel_list(lfpfile,lfp_files(i).folder);
+        write_xml(lfp_files(i).folder,probe_map,Fold,Fnew)
+    end
+    
     % preallocate & get time stamps
     [ts] = Nlx2MatCSC(lfpfile{1},[1 0 0 0 0], 0, 1, [] );
     signal = zeros(length(lfpfile),(length(ts)*512)*Fnew/Fold);
@@ -31,7 +41,7 @@ for i = 1:length(lfp_files)
     
     % save .lfp
     disp('saving .lfp file');
-    fidout = fopen(fullfile(lfp_files(i).folder,lfp_files(i).name), 'w');
+    fidout = fopen(fullfile(lfp_files(i).folder,[basename,'.lfp']), 'w');
     fwrite(fidout,signal,'int16');
     fclose(fidout);
     WaitMessage.Send;
@@ -44,4 +54,33 @@ channels=table2cell(struct2table(dir([session_path,filesep,'*.ncs'])));
 lfpfile=strcat(channels(:,2),filesep,channels(:,1));
 [~,idx]=sort(str2double(extractBetween(lfpfile,'CSC','.ncs')));
 lfpfile = lfpfile(idx);
+end
+
+function write_xml(path,map,Fold,FNew)
+% Wrapper for buzcode bz_MakeXMLFromProbeMaps with defaults
+
+defaults.NumberOfChannels = 1;
+defaults.SampleRate = Fold;
+defaults.BitsPerSample = 16;
+defaults.VoltageRange = 20;
+defaults.Amplification = 1000;
+defaults.LfpSampleRate = FNew;
+defaults.PointsPerWaveform = 64;
+defaults.PeakPointInWaveform = 32;
+defaults.FeaturesPerWave = 4;
+[~,basename] = fileparts(path);
+bz_MakeXMLFromProbeMaps({map},path,basename,1,defaults)
+end
+
+function probe_map = get_channel_list(lfpfile,path)
+
+% many session may only have one continuous 'lfp' channel per tetrode
+if length(dir(fullfile(path,'*.ncs'))) ==...
+        length(dir(fullfile(path,'*.ntt')))
+    probe_map = {[num2str(max(str2double(extractBetween(lfpfile,'CSC','.ncs')))),...
+        'tt_without_all_channels.xlsx']};
+else
+    probe_map = {[num2str(max(str2double(extractBetween(lfpfile,'CSC','.ncs')))/4),...
+        'tt.xlsx']};
+end
 end
