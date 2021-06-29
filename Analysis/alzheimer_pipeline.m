e2781e86a87564f9e2d798b839d46da6cedddffa
BClarkLab <BClarkLab@PSYCH-DGR91CH2.colleges.ad.unm.edu>
2021-06-29 13:18:17 -0600

    updated with new rats

--- Analysis/alzheimer_pipeline.m
+++ Analysis/alzheimer_pipeline.m
@@ -14,31 +14,38 @@ addpath('F:\ClarkP30_Recordings\AnimalMetaData');
 addpath('F:\ClarkP30_Recordings\Analysis\')
 
 %% Assign location to save data and to find data
-save_path = 'F:\ClarkP30_Recordings\Analysis\GLM\';
 dataset = 'F:\ClarkP30_Recordings\ProcessedData\';
 
 %% Assign rats to include in analysis (if not assigned, will default to all
 % rats. 
- rats={'LB03','LB04','LB07','LB10'}; 
- transgenic = {'LB04','LB07','LB10'};
+ rats = {'LB03','LB04','LB07','LB10','LB11','PoS4','PoS3'}; 
+ transgenic = {'LB04','LB07','LB10','PoS3'};
+ 
 %% Run compile cell measures to obtain ID of all units with min 100 spikes &
 % 1Hz peak firing rate. 
 compile_cell_measures(rats,transgenic)
 
-%% Runs through Hardcastle LN GLM for HD,Place, and HD/Place(Full model)
-batch_hd_glm(dataset,save_path,'F:\ClarkP30_Recordings\Analysis\cell_list.csv')
+%% Import cell_list and create index .csv using find cells (For python processing)
+cell_list = readtable('D:/Users/BClarkLab/github/Berkowitz_et_al_2021/Cell_Classification/data/cell_list.csv');
+cell_list.cell_idx{1} = [];
 
-%% Import hd_cell_list and create index .csv using find cells (For python processing)
-hd = readtable('F:\ClarkP30_Recordings\Analysis\hd_cell_list.csv');
-cell_idx = table;
-cd 'F:\ClarkP30_Recordings\ProcessedData\'
-for i = 1 : length(hd.SessionID)
-    data = load(['F:\ClarkP30_Recordings\ProcessedData\',hd.SessionID{i}],'spikesID');
-    tet = sscanf(hd.tetrode{i},'TT%d.mat');
-    cell_idx.n(i,1) = find_cells(data,tet,hd.cell(i));
+for i = 1 : length(cell_list.session)
+    data = load(['d:\Users\BClarkLab\GoogleDrive_\ClarkP30_Recordings\ProcessedData\',cell_list.session{i}],'spikesID');
+    tet = sscanf(cell_list.tetrode{i},'TT%d.mat');
+    cell_list.cell_idx{i,1} = find_cells(data,tet,cell_list.cell(i));
 end
+% convert cell to double
+cell_list.cell_idx = cell2mat(cell_list.cell_idx);
+% write back to list
+writetable(cell_list,'D:/Users/BClarkLab/github/Berkowitz_et_al_2021/Cell_Classification/data/cell_list.csv')
 
-writetable(cell_idx,['F:\ClarkP30_Recordings\Analysis\','hd_cell_spike_idx.csv'])
+%% Calculate metrics for cell classification using cell list
+classify_pyr_int
+
+%% Runs through Hardcastle LN GLM for HD,Place, and HD/Place(Full model)
+% batch_hdVel_glm(dataset,save_path,'D:\Analysis\cell_list.csv')
+
+
 %% For all cells classified as HD, find additional measures of interest,
 % compile, and save as csv for analysis in R or Python. Saves copy to analysis folder and keeps data_all 
 % (long format of all measures across max 4 conditions) in workspace. 
