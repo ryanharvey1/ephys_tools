0bcb3c06e91c0f67cd86e724480eb31da8f65fa0
Laura Berkowitz <leb239@cornell.edu>
2022-08-16 11:18:58 -0400

    added clarkp30 paths

--- Analysis/make_cell_list.m
+++ Analysis/make_cell_list.m
@@ -1,6 +1,6 @@
 % make_cell_list
 
-data_path = 'F:\Projects\PAE_PlaceCell\ProcessedData\';
+data_path = 'F:\ClarkP30_Recordings\ProcessedData\';
 
 sessions = dir(fullfile(data_path,'*.mat'));
 sessions = struct2table(sessions);
@@ -15,7 +15,7 @@ for i = 1:length(sessions.name)
         cellstr(num2str(data.spikesID.CellNum))];
     group_id = [group_id;temp_id];
 end
-group_id=get_region_id(group_id,'F:\Projects\PAE_PlaceCell\AnimalMetadata');
+group_id=get_region_id(group_id,'F:\ClarkP30_Recordings\AnimalMetaData');
 % make dg into ca3
 for i=1:length(group_id)
     if strcmp(group_id{i,end},'dg')
@@ -29,4 +29,4 @@ group_id.session = extractBefore(group_id.session,'.mat');
 group_id.cell = str2double(group_id.cell);
 
 
-writetable(group_id,'F:\Projects\PAE_PlaceCell\analysis\cell_list.csv')
+writetable(group_id,'F:\ClarkP30_Recordings\analysis/cell_list.csv')
